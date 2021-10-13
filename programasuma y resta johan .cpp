17a39d14495eb295bb6bd4417e0af9f9c6aea3d9
JohanMusic05 <89704545+electroJohanHe05@users.noreply.github.com>
2021-10-13 16:43:50 +0000

    Initial commit

---
+++ programasuma y resta johan .cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int num_1=0;
+int num_2=0;
+int result_1=0;
+cout <<"escribir numero:  ";
+cin  >> num_1;
+cout <<"escribir numero:  ";
+cin  >> num_2;
+result_1 =num_1+num_2 ;
+cout<< result_1 <<"\n";
+cout <<"escribir numero:  ";
+cin  >> num_1;
+cout <<"escribir numero:  ";
+cin  >> num_2;
+result_1 =num_1-num_2 ;
+cout<< result_1;
+}
